433eefd9c6821c548b7665201382dbdf98c326e0
shahid chaudhary <shahidchaudhary583@gmail.com>
2023-12-06 16:32:53 +0530

    dataypes

---
+++ 01basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 2345
+let accountEmail = "shahidchaudhary583@gmail.com"
+var accountPasswork = "123456"
+accountCity = "Bilaspur"
+let accountstate
+
+accountPasswork = "0258"
+accountEmail = "ramu@gmail.com"
+
+console.table([accountEmail,accountId,accountPasswork,accountCity,accountstate]);
---
+++ 01basics/02_datatypes.js
@@ -0,0 +1,22 @@
+// "use strict"
+
+let name = "shahid"
+let age = 18
+let islogIn = true
+// Types of data types =>
+
+//  number
+//  bigint  
+//  string
+//  boolean
+// undefined
+
+// null 
+
+// object
+
+
+
+
+console.log(typeof 45);
+console.log(typeof null); 
