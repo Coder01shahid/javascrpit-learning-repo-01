78c6a4c68ca1d5fd7a75fb73d0bfb433e546d490
shahid chaudhary <shahidchaudhary583@gmail.com>
2023-12-07 11:31:12 +0530

    conervsion of datatypes

---
+++ 01basics/03_conversionOperation.js
@@ -0,0 +1,23 @@
+let score = "33"
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log( valueInNumber);
+
+                    // type conversion of datatypes
+// "33" => 33
+// "33abc" => NaN (not a number)
+// null => 0
+// undefined => NaN
+// boolean => ture:1 and faluse:0
+// string => NaN
+
+
+let IsLogedIn = 1
+let islogIn = ""
+
+let booleanLoggedIn = Boolean(IsLogedIn)
+console.log(typeof IsLogedIn);
+console.log(typeof booleanLoggedIn);
+console.log(booleanLoggedIn);
+console.log(typeof islogIn);
