580ce04306dcb13c0147185a7bb3b5d32aa90019
shahid chaudhary <shahidchaudhary583@gmail.com>
2023-12-07 12:00:35 +0530

    operations

--- 01basics/03_conversionOperation.js
+++ 01basics/03_conversionOperation.js
@@ -1,8 +1,8 @@
 let score = "33"
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log( valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log( valueInNumber);
 
                     // type conversion of datatypes
 // "33" => 33
@@ -17,7 +17,34 @@ let IsLogedIn = 1
 let islogIn = ""
 
 let booleanLoggedIn = Boolean(IsLogedIn)
-console.log(typeof IsLogedIn);
-console.log(typeof booleanLoggedIn);
-console.log(booleanLoggedIn);
-console.log(typeof islogIn);
+// console.log(typeof IsLogedIn);
+// console.log(typeof booleanLoggedIn);
+// console.log(booleanLoggedIn);
+// console.log(typeof islogIn);
+
+
+//*********Operation********* */
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " shahid"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1"+ 2);
+// console.log(1+ "2");
+// console.log(1+2+"3");
+
+// console.log((3+2)*5%3); //valued form of code in JS
+
+
+
+
+
