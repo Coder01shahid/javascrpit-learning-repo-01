4491da1e85ef70169e6369f0458f83575aece4c4
shahid chaudhary <shahidchaudhary583@gmail.com>
2023-12-08 11:12:36 +0530

    datatypes summary

---
+++ 01basics/04_dataTypesSummary.js
@@ -0,0 +1,29 @@
+// Primative data types
+// call by value :- we take data by copying not allow them to use memory location.
+
+// types(6):- String, null,Number,undefined,boolean,symbol,BigIn(.)
+
+
+// refrence data types:- array, objects and functions.
+
+// If you want the mastery of js then master [ OBJECTS and borwsor event ]
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId);
+
+// how we define array ,object and function in variable
+
+const heros = ["Shaktiman", "chotabheam","nagraz"]
+
+let myobj = {
+    myName:"shahid",
+    myAge: 22,
+}
+
+
+
+// lets define a Function
+
+
+
