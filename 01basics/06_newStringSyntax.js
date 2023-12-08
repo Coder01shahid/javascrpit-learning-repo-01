be2bc21bf625bfee64aa24bad15d8ba03ab03b63
shahid chaudhary <shahidchaudhary583@gmail.com>
2023-12-08 15:55:33 +0530

    strings protocol

---
+++ 01basics/06_newStringSyntax.js
@@ -0,0 +1,16 @@
+let name ="shahid"
+let repoCount = 50
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+
+const gameName = new String('shahid')
+console.log(gameName)
+console.log(gameName.toUpperCase());
+
+// For avoiding extra space in input form then use TRIM
+
+const myString = "    shahid   "
+console.log(myString);
+console.log(myString.trim());
+// learn about strings from mdn
